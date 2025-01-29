9fd7667b049ef20922fd7ca10a25b59ff61d9192
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2025-01-29 14:36:19 +0700

    Add fourth project

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,56 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(MAIN_LOOP)
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ
+  @BLACK
+  0;JMP
+
+(PAINT)
+  @8192
+  D=A
+  @i
+  M=D
+(PAINT_LOOP)
+  @SCREEN
+  D=A
+  @i
+  D=D+M
+  @pointer
+  M=D
+  @color
+  D=M
+  @pointer
+  A=M
+  M=D
+  @i
+  M=M-1
+  D=M
+  D=D+1
+  @MAIN_LOOP
+  D;JEQ
+  @PAINT_LOOP
+  0;JMP
+
+(BLACK)
+  @color
+  M=-1
+  @PAINT
+  0;JMP
+
+(WHITE)
+  @color
+  M=0
+  @PAINT
+  0;JMP
+
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,42 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Set @i to the value to R0
+  @R0
+  D=M
+  @i
+  M=D
+
+// Set @sum to 0
+  @sum
+  M=0
+
+// Loop to multiply
+(LOOP)
+  @i
+  D=M
+  @SET_OUTPUT
+  D;JEQ
+  D=D-1
+  @i
+  M=D
+  @R1
+  D=M
+  @sum
+  M=D+M
+  @LOOP
+  0;JMP
+
+(SET_OUTPUT)
+  @sum
+  D=M
+  @R2
+  M=D
+  @END
+  0;JMP
+
+// End Loop
+(END)
+  @END
+  0;JMP
+
